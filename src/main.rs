3bac73a740ae37a672474670c54d1c7591cc1a4c
xuyanwen2012 <info@chivalrystudio.com>
2020-04-04 17:03:26 -0700

    fresh init

--- src/main.rs
+++ src/main.rs
@@ -1,96 +1,108 @@
-use std::time::{Duration, Instant};
-
-use crate::server::Connect;
 use actix::*;
 use actix_files as fs;
-use actix_web::{middleware::Logger, web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
 use actix_web_actors::ws;
+use std::time::{Duration, Instant};
 
-mod codec;
 mod server;
-mod session;
 
-/// Entry point for our route
-// async fn chat_route(
-//     req: HttpRequest,
-//     stream: web::Payload,
-//     srv: web::Data<Addr<server::ChatServer>>,
-// ) -> Result<HttpResponse, Error> {
-//     ws::start(
-//         WsChatSession {
-//             id: 0,
-//             hb: Instant::now(),
-//             addr: srv.get_ref().clone(),
-//         },
-//         &req,
-//         stream,
-//     )
-// }
+/// How often heartbeat pings are sent
+const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
+/// How long before lack of client response causes a timeout
+const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
 
-struct WsDummySession {
-    /// unique session id
-    id: usize,
-    // /// Client must send ping at least once per 10 seconds (CLIENT_TIMEOUT),
-    // /// otherwise we drop connection.
-    // hb: Instant,
-    /// Chat server
-    addr: Addr<server::DummyServer>,
+async fn game_route(
+    req: HttpRequest,
+    stream: web::Payload,
+    srv: web::Data<Addr<server::GameServer>>,
+) -> Result<HttpResponse, actix_web::Error> {
+    ws::start(
+        WsSession {
+            id: 0,
+            hb: Instant::now(),
+            addr: srv.get_ref().clone(),
+        },
+        &req,
+        stream,
+    )
 }
 
-impl Actor for WsDummySession {
+// Define http actor
+struct WsSession {
+    /// unique session id
+    id: usize,
+    /// Client must send ping at least once per 10 seconds (CLIENT_TIMEOUT),
+    /// otherwise we drop connection.
+    hb: Instant,
+    /// Reference to the GameServer
+    addr: Addr<server::GameServer>,
+}
+
+impl Actor for WsSession {
     type Context = ws::WebsocketContext<Self>;
 
     /// Method is called on actor start.
+    /// We register ws session with GameServer
     fn started(&mut self, ctx: &mut Self::Context) {
-        let addr = ctx.address();
-        addr.send(server::Connect {
-            addr: addr.recipient(),
-        })
-        .into_actor(self)
-        .then(|res, act, ctx| {
-            match res {
-                Ok(res) => act.id = res,
-                // something is wrong with chat server
-                _ => ctx.stop(),
-            }
-        })
-        .wait(ctx)
+        // we'll start heartbeat process on session start.
+        self.hb(ctx);
     }
 
     fn stopping(&mut self, ctx: &mut Self::Context) -> Running {
-        unimplemented!()
+        // notify chat server
+        // self.addr.do_send(server::Disconnect { id: self.id });
+        Running::Stop
     }
 }
 
-impl Handler<session::Message> for WsDummySession {
-    type Result = ();
+impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WsSession {
+    fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
+        match msg {
+            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
+            Ok(ws::Message::Text(text)) => ctx.text(text),
+            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
+            _ => (),
+        }
+    }
+}
 
-    fn handle(&mut self, msg: session::Message, ctx: &mut Self::Context) -> Self::Result {
-        unimplemented!()
+impl WsSession {
+    /// helper method that sends ping to client every second.
+    /// also this method checks heartbeats from client.
+    fn hb(&self, ctx: &mut ws::WebsocketContext<Self>) {
+        ctx.run_interval(HEARTBEAT_INTERVAL, |act, ctx| {
+            if Instant::now().duration_since(act.hb) > CLIENT_TIMEOUT {
+                // heartbeat timed out
+                println!("Websocket Client heartbeat failed, disconnecting!");
+
+                // notify chat server
+                // act.addr.do_send(server::Disconnect { id: act.id });
+
+                // stop actor
+                ctx.stop();
+
+                // don't try to send a ping
+                return;
+            }
+
+            ctx.ping(b"");
+        });
     }
 }
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
 
-    // Start server actor
-    let server = server::DummyServer::default().start();
-
-    // Start tcp server in separate thread
-    let srv = server.clone();
-    session::tcp_server("127.0.0.1:12345", srv);
-
-    println!("Started http server: 127.0.0.1:8080");
+    // Start chat server actor
+    let server = server::GameServer::default().start();
 
     // Create Http server with websocket support
     HttpServer::new(move || {
         App::new()
-            .wrap(Logger::default())
             .data(server.clone())
             // websocket
-            // .service(web::resource("/ws/").to(chat_route))
+            .service(web::resource("/ws/").to(game_route))
             // static resources
             .service(fs::Files::new("/", "static/").index_file("index.html"))
     })
--- src/server.rs
+++ src/server.rs
@@ -1,82 +1,22 @@
 use actix::prelude::*;
-
 use rand::{self, rngs::ThreadRng, Rng};
-use std::collections::HashSet;
 
-use crate::session;
-
-/// Message for chat server communications
-
-/// New chat session is created
-#[derive(Message)]
-#[rtype(usize)]
-pub struct Connect {
-    pub addr: Recipient<session::Message>,
-}
-
-/// Session is disconnected
-#[derive(Message)]
-#[rtype(result = "()")]
-pub struct Disconnect {
-    pub id: usize,
-}
-
-/// This is a dummy server for leaning and testing, only holding the connected sessions.
-///
-pub struct DummyServer {
-    sessions: HashSet<usize>,
+/// `GameServer` manages chat rooms and responsible for coordinating chat
+/// session. implementation is super primitive
+pub struct GameServer {
+    // sessions: HashMap<usize, Recipient<Message>>,
+    // rooms: HashMap<String, HashSet<usize>>,
     rng: ThreadRng,
 }
 
-impl Default for DummyServer {
+impl Default for GameServer {
     fn default() -> Self {
-        DummyServer {
-            sessions: HashSet::new(),
+        GameServer {
             rng: rand::thread_rng(),
         }
     }
 }
 
-/// Handler for Connect message.
-///
-/// Register new session and assign unique id to this session
-impl Handler<Connect> for DummyServer {
-    type Result = usize;
-
-    fn handle(&mut self, msg: Connect, _: &mut Context<Self>) -> Self::Result {
-        // TODO: notify all users in same room
-        // self.send_message(&"Main".to_owned(), "Someone joined", 0);
-
-        let id = self.rng.gen::<usize>();
-        self.sessions.insert(id);
-
-        println!("{} joined", id);
-
-        // send id back
-        id
-    }
-}
-
-/// Handler for Disconnect message.
-impl Handler<Disconnect> for DummyServer {
-    type Result = ();
-
-    fn handle(&mut self, msg: Disconnect, _: &mut Context<Self>) -> Self::Result {
-        println!("{} disconnected", msg.id);
-        self.sessions.remove(&msg.id);
-    }
-}
-
-impl DummyServer {
-    fn list_sessions(&self) {
-        println!("Current list of session has: ");
-        for id in self.sessions.iter() {
-            println!("{}", id);
-        }
-    }
-}
-
-/// Make actor from `DummyServer`
-impl Actor for DummyServer {
+impl Actor for GameServer {
     type Context = Context<Self>;
 }
