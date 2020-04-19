96a25f24b1cf23f0aa43ab4a245bd78deaf881e0
xuyanwen2012 <info@chivalrystudio.com>
2020-04-19 11:30:13 -0700

    hyper init

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,29 @@
-fn main() {}
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server};
+use std::convert::Infallible;
+use std::net::SocketAddr;
+use tokio::net::TcpListener;
+use tokio::prelude::*;
+
+async fn hello_world(_: Request<Body>) -> Result<Response<Body>, Infallible> {
+    Ok(Response::new("Hello World".into()))
+}
+
+#[tokio::main]
+async fn main() {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    // A `Service` is needed for every connection, so this
+    // creates one from our `hello_world` function.
+    let make_svc = make_service_fn(|_| async {
+        // service_fn converts our function into a `Service`
+        Ok::<_, Infallible>(service_fn(hello_world))
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    // Run this server for... forever!
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
+}
